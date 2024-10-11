3966973f6e8c9c9ba31a0abb8fd413cd9e4eb860
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2024-10-11 23:17:22 +0300

    lang toggler

--- js/langToggler.js
+++ js/langToggler.js
@@ -1,9 +1,12 @@
 const langButton = document.querySelector(".lang-btn");
 const langModal = document.querySelector(".lang-btn-modal");
+const langIcon = document.querySelector('.lang-btn-icon')
 
 const isLangModalOpen = false;
 
 const toggleLangModal = () => {
+    langModal.classList.toggle('active')
+    langIcon.classList.toggle('open')
   console.log('toogle');
 };
 
